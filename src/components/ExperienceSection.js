29468d717e7111be1cf4a543f33f2afddbeea816
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-08-06 15:09:33 +0100

    corr

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -12,49 +12,49 @@ import { SiPostman } from "react-icons/si";
 
 const ExperienceSection = () => {
   return (
-    <div className="bg-slate-100 p-10">
+    <div className="bg-slate-100 md:p-10">
       <div className="text-center mt-[2rem] font-semibold font-mono text-lg">
         <p>Explore My</p>
       </div>
-      <div className="text-center text-3xl mt-5 font-medium font-sans">
+      <div className="text-center text-3xl mt-2 font-medium font-sans">
         <h1>Experiences</h1>
       </div>
       <div className="flex justify-center">
-        <div className="flex-col lg:flex justify-center lg:flex-row gap-8 mt-20">
-          <div className="sm:w-[20rem] border-2 border-black rounded-3xl md:w-[20rem] h-56">
+        <div className="flex-col lg:flex justify-center lg:flex-row gap-8 mt-8">
+          <div className="sm:w-[20rem] p-5 border-2 border-black rounded-3xl md:w-[20rem] h-56">
             <div className="text-center">
-              <h1 className="text-2xl mt-5 font-bold">Frontend Development</h1>
+              <h1 className="md:text-2xl mt-5 font-bold">Frontend Development</h1>
               <div className="mt-3">
-                <div className="flex justify-center gap-14">
+                <div className="flex justify-center md:gap-14 gap-10">
                   <div>
                     <div className="flex gap-1">
-                      <FaHtml5 class="text-[20px] text-orange-500" />
-                      <p className="font-bold text-[14px]">HTML</p>
+                      <FaHtml5 class="md:text-[20px] text-orange-500" />
+                      <p className="font-bold md:text-[14px] text-[11px]">HTML</p>
                     </div>
-                    <span className="text-[14px] ml-2">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                   <div className="">
                     <div className="flex gap-1">
-                      <SiTailwindcss class="text-[20px] text-blue-500" />
-                      <p className="font-bold text-[14px]">CSS/Tailwind</p>
+                      <SiTailwindcss class="md:text-[20px] text-blue-500" />
+                      <p className="font-bold md:text-[14px] text-[11px]">CSS/Tailwind</p>
                     </div>
-                    <span className="text-[14px] ml-3">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                 </div>
-                <div className="flex justify-center gap-14 mt-10">
+                <div className="flex justify-center md:gap-14 gap-10 mt-10">
                   <div>
                     <div className="flex gap-2">
-                      <RiJavascriptLine class="text-[23px] text-yellow-400" />
-                      <p className="font-bold text-[14px]">JavaScript</p>
+                      <RiJavascriptLine class="md:text-[23px] text-yellow-400" />
+                      <p className="font-bold md:text-[14px] text-[11px]">JavaScript</p>
                     </div>
-                    <span className="text-[14px] ml-3">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                   <div className="">
                     <div className="flex gap-2">
-                      <FaReact class="text-[20px] text-blue-500" />
-                      <p className="font-bold text-[14px]">React</p>
+                      <FaReact class="md:text-[20px] text-blue-500" />
+                      <p className="font-bold md:text-[14px] text-[11px]">React</p>
                     </div>
-                    <span className="text-[14px] ml-3">Intermediate</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Intermediate</span>
                   </div>
                 </div>
               </div>
@@ -62,64 +62,64 @@ const ExperienceSection = () => {
           </div>
 
 
-          <div className="sm:w-[20rem] border-2 border-black rounded-3xl md:w-[20rem] h-56">
+          <div className="sm:w-[20rem] border-2 border-black rounded-3xl md:w-[20rem] h-56 md:mt-0 mt-7">
             <div className="text-center">
-              <h1 className="text-2xl mt-5 font-bold">Backend Development</h1>
+              <h1 className="md:text-2xl mt-5 font-bold">Backend Development</h1>
               <div className="mt-3">
-                <div className="flex justify-center gap-14">
+                <div className="flex justify-center md:gap-14 gap-10">
                   <div>
                     <div className="flex gap-1">
-                      <FaNode class="text-[23px] text-green-500" />
-                      <p className="font-bold text-[14px]">Node</p>
+                      <FaNode class="md:text-[23px] text-green-500" />
+                      <p className="font-bold md:text-[14px] text-[11px]">Node</p>
                     </div>
-                    <span className="text-[14px] ml-2">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                   <div className="">
                     <div className="flex gap-1">
-                      <SiMysql class="text-[23px] text-blue-900" />
-                      <p className="font-bold text-[14px]">MySQL</p>
+                      <SiMysql class="md:text-[23px] text-blue-900" />
+                      <p className="font-bold md:text-[14px] text-[11px]">MySQL</p>
                     </div>
-                    <span className="text-[14px] ml-3">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                 </div>
-                <div className="flex justify-center gap-14 mt-10">
+                <div className="flex justify-center md:gap-14 gap-10 mt-10">
                   <div>
-                    <div className="flex gap-3">
-                      <SiPhp class="text-[20px] text-purple-600" />
-                      <p className="font-bold text-[14px]">PHP</p>
+                    <div className="flex md:gap-3 gap-1">
+                      <SiPhp class="md:text-[20px] text-purple-600" />
+                      <p className="font-bold md:text-[14px] text-[11px]">PHP</p>
                     </div>
-                    <span className="text-[14px] ml-3">Intermediate</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Intermediate</span>
                   </div>
                   <div className="">
-                    <div className="flex gap-3">
-                      <AiOutlinePython class="text-[20px] text-yellow-500" />
-                      <p className="font-bold text-[14px]">Python</p>
+                    <div className="flex md:gap-3 gap-1">
+                      <AiOutlinePython class="md:text-[20px] text-yellow-500" />
+                      <p className="font-bold md:text-[14px] text-[11px]">Python</p>
                     </div>
-                    <span className="text-[14px] ml-3">Basic</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Basic</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="sm:w-[20rem] border-2 border-black rounded-3xl md:w-[20rem] h-56">
+          <div className="sm:w-[20rem] border-2 border-black rounded-3xl md:w-[20rem] md:h-56 h-[8rem] md:mt-0 mt-7">
             <div className="text-center">
-              <h1 className="text-2xl mt-5 font-bold">Others</h1>
+              <h1 className="md:text-2xl mt-5 font-bold">Others</h1>
               <div className="mt-3">
-                <div className="flex justify-center gap-14">
+                <div className="flex justify-center md:gap-14 gap-8">
                   <div>
                     <div className="flex gap-1">
-                      <FaGithub class="text-[20px] text-" />
-                      <p className="font-bold text-[14px]">Github</p>
+                      <FaGithub class="md:text-[20px] text-" />
+                      <p className="font-bold md:text-[14px] text-[11px]">Github</p>
                     </div>
-                    <span className="text-[14px] ml-2">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                   <div className="">
                     <div className="flex gap-1">
-                      <SiPostman class="text-[20px] text-orange-500"  />
-                      <p className="font-bold text-[14px]">Postman</p>
+                      <SiPostman class="md:text-[20px] text-orange-500"  />
+                      <p className="font-bold md:text-[14px] text-[11px]">Postman</p>
                     </div>
-                    <span className="text-[14px] ml-3">Experienced</span>
+                    <span className="md:text-[14px] text-[12px] md:ml-2 ml-[1rem]">Experienced</span>
                   </div>
                 </div>
                 {/* <div className="flex justify-center gap-14 mt-10">
