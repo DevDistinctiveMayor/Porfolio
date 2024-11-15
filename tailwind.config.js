405742a15cdd954f3c48c6d2bc127b1d2664d8f1
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-11-15 15:10:41 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Updated from "purge" to "content"
   theme: {
     extend: {
       colors: {
         primary: '#B8B099',
         secondary: {
-          
           400: '#243c5a',
         },
       },
