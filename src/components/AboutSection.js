004fab9e4b54aed03dcd2c5cbf5fb2d993c7d6f0
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2025-07-30 20:52:29 +0100

    data

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,22 +5,23 @@ import img1 from '../img/WhatsApp Image 2024-02-28 at 22.35.37_e82063ca.jpg'
 
 const AboutSection = () => {
   return (
-    <div className="bg-gradient-to-r from-[#033a66] to-slate-400 p-10 pb-[10rem] text-slate-100">
-      <div className="text-center mt-[2rem] font-semibold font-mono">
-        <p>Get To Know More</p>
-      </div>
-      <div className="text-center text-3xl mt-5 font-medium font-sans">
-        <h1>About Me</h1>
-      </div>
-      <div className="relative flex justify-center">
-        <div className="mt-20 place-content-center flex flex-col gap-9 md:flex-row md:justify-center">
-          <div className="w-64">
-            <img src={img1} alt="Mayor" className="rounded-3xl" />
-            <p className="mt-4 md:w-[34rem] md:ml-72 md:-mt-40">
-              I am Ekundayo Mayowa Timileyin, a developer with a fervent enthusiasm for crafting elegant solutions to complex problems in the digital world. Here's a glimpse into my journey and what drives me:
+    <div className="bg-gradient-to-r from-[#033a66] to-slate-400 text-slate-100 pb-10">
+      <div className="text-center font-semibold font-mono flex justify-center">
+
+        <div className="w-[34rem]">
+                  <p>Get To Know More</p>
+            {/* <img src={img1} alt="Mayor" className="rounded-3xl" /> */}
+            <p className="md:w-[34rem] mt-5">
+              I am a Developer with a fervent enthusiasm for crafting elegant solutions to complex problems in the digital world. Here's a glimpse into my journey and what drives me:
             </p>
           </div>
-          <div className="border-2 border-white rounded-3xl w-60 h-32">
+      </div>
+      
+  
+      <div className="justify-center">
+        <div className="flex justify-center gap-10 mt-8">
+          
+          <div className="border-2 border-white rounded-[10px] w-60 h-32">
             <div className="grid place-items-center mt-4">
               <FaBook class="text-[20px]"/>
               <p className="text-xl font-bold">Experience</p>
@@ -28,7 +29,8 @@ const AboutSection = () => {
               <p>Full-Stack Developer</p>
             </div>
           </div>
-          <div className="border-2 border-white rounded-3xl w-60 h-32">
+
+          <div className="border-2 border-white rounded-[10px] w-60 h-32">
             <div className="grid place-items-center mt-4">
               <FaRegLightbulb class="text-[20px]"/>
               <p className="text-xl font-bold">Education</p>
