c781f7e4befcb859825d2678546a5c2617f748dc
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2025-07-31 23:46:37 +0100

    data

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,47 +5,44 @@ import { FaRegLightbulb } from "react-icons/fa";
 
 const AboutSection = () => {
   return (
-   <div className="bg-gradient-to-r from-[#0f172a] via-[#1e293b] to-[#334155] pb-10">
-  <div className="text-center font-semibold font-mono flex justify-center">
-    <div className="w-[21rem] sm:w-[34rem] mt-4">
-      <p className="text-sky-400 text-[22px]">Get To Know More</p>
-      {/* <img src={img1} alt="Mayor" className="rounded-3xl" /> */}
-      <p className="text-slate-200 mt-20 leading-relaxed">
-As a developer, I’m driven by a deep passion for crafting thoughtful, scalable solutions to complex digital problems. Here’s a look into the experiences and values that shape my work:
-      </p>
-    </div>
-  </div>
+    <div className="bg-gradient-to-r from-[#0f172a] via-[#1e293b] to-[#334155] pb-10">
+      <div className="text-center font-semibold font-mono flex justify-center">
+        <div className="w-[21rem] sm:w-[34rem] mt-4">
+          <p className="text-sky-400 text-[22px]">Get To Know More</p>
+          {/* <img src={img1} alt="Mayor" className="rounded-3xl" /> */}
+          <p className="text-slate-200 mt-20 leading-relaxed">
+            As a developer, I’m driven by a deep passion for crafting
+            thoughtful, scalable solutions to complex digital problems. Here’s a
+            look into the experiences and values that shape my work:
+          </p>
+        </div>
+      </div>
 
-  <div className="flex justify-center text-slate-100">
-  <div className="flex justify-center gap-10 mt-10 flex-col sm:flex-row font-mono">
+      <div className="flex justify-center text-slate-100">
+        <div className="flex justify-center gap-10 mt-10 flex-col sm:flex-row font-mono">
+          {/* Experience Card */}
+          <div className="border border-sky-500 bg-[#1e293b]/50 backdrop-blur-sm rounded-xl w-[17rem] sm:w-60 h-[9rem] shadow-md hover:shadow-sky-500 transition">
+            <div className="grid place-items-center mt-4 text-center">
+              <FaBook className="text-[22px] text-sky-400" />
+              <p className="text-xl font-bold mt-1">Experience</p>
+              <span className="text-sm">3 + Years</span>
+              <p className="text-sm">Full-Stack Developer</p>
+            </div>
+          </div>
 
-    {/* Experience Card */}
-    <div className="border border-sky-500 bg-[#1e293b]/50 backdrop-blur-sm rounded-xl w-[17rem] sm:w-60 h-[9rem] shadow-md hover:shadow-sky-500 transition">
-      <div className="grid place-items-center mt-4 text-center">
-        <FaBook className="text-[22px] text-sky-400" />
-        <p className="text-xl font-bold mt-1">Experience</p>
-        <span className="text-sm">3 + Years</span>
-        <p className="text-sm">Full-Stack Developer</p>
+          {/* Education & Certifications Card */}
+          <div className="border border-sky-500 bg-[#1e293b]/50 backdrop-blur-sm rounded-xl w-[17rem] sm:w-60 h-[9rem] shadow-md hover:shadow-sky-500 transition">
+            <div className="grid place-items-center mt-4 text-center">
+              <FaRegLightbulb className="text-[22px] text-sky-400" />
+              <p className="text-xl font-bold mt-1">Education</p>
+              <span className="text-sm">B.A. English (Education)</span>
+              <span className="text-sm">Full-Stack Dev – Aptech</span>
+              <span className="text-sm">Cert: Python, Soft Skills</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    {/* Education & Certifications Card */}
-    <div className="border border-sky-500 bg-[#1e293b]/50 backdrop-blur-sm rounded-xl w-[17rem] sm:w-60 h-[9rem] shadow-md hover:shadow-sky-500 transition">
-      <div className="grid place-items-center mt-4 text-center">
-        <FaRegLightbulb className="text-[22px] text-sky-400" />
-        <p className="text-xl font-bold mt-1">Education</p>
-        <span className="text-sm">B.A. English (Education)</span>
-        <span className="text-sm">Full-Stack Dev – Aptech</span>
-        <span className="text-sm">Cert: Python, Soft Skills</span>
-      </div>
-    </div>
-
-  </div>
-</div>
-
-  </div>
-
-
   );
 };
 
--- src/components/ProfileSection.js
+++ src/components/ProfileSection.js
@@ -4,53 +4,55 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const ProfileSection = () => {
   return (
-<div className="bg-gradient-to-r from-[#0f172a] via-[#1e293b] to-[#334155] p-10">
-  <div className="grid mt-[7rem] place-items-center md:grid-flow-col justify-center gap-10">
-    <div>
-      <img
-        src={img1}
-        alt="MAYOR"
-        className="h-72 mx-auto object-fill rounded-full border-4 border-slate-300"
-      />
-    </div>
+    <div className="bg-gradient-to-r from-[#0f172a] via-[#1e293b] to-[#334155] p-10">
+      <div className="grid mt-[7rem] place-items-center md:grid-flow-col justify-center gap-10">
+        <div>
+          <img
+            src={img1}
+            alt="MAYOR"
+            className="h-72 mx-auto object-fill rounded-full border-4 border-slate-300"
+          />
+        </div>
 
-    <div className="text-center mt-10 font-mono md:-mt-10">
-<h1 className="text-slate-300 text-lg">Hello, I’m</h1>
-<h2 className="text-2xl font-serif font-semibold mt-2 text-white">Mayowa Ekundayo</h2>
-<h3 className="text-lg mt-2 text-slate-300">Full-Stack Developer | Passionate Problem Solver</h3>
+        <div className="text-center mt-10 font-mono md:-mt-10">
+          <h1 className="text-slate-300 text-lg">Hello, I’m</h1>
+          <h2 className="text-2xl font-serif font-semibold mt-2 text-white">
+            Mayowa Ekundayo
+          </h2>
+          <h3 className="text-lg mt-2 text-slate-300">
+            Full-Stack Developer | Passionate Problem Solver
+          </h3>
 
+          <div className="flex justify-center mt-3 gap-4">
+            <a
+              href="/Cv folder/Ekundayo_mayowa_T.pdf"
+              className="border-2 border-sky-500 rounded-xl px-4 py-1 text-sky-500 hover:bg-sky-500 hover:text-white transition"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View CV
+            </a>
+            <a
+              href="/Cv folder/Cover_Letter.pdf"
+              className="rounded-xl px-4 py-1 bg-sky-500 text-white hover:bg-sky-600 transition"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Cover Letter
+            </a>
+          </div>
 
-      <div className="flex justify-center mt-3 gap-4">
-        <a
-          href="/Cv folder/EKUNDAYO MAYOWA TIMILEYIN UPDATED.pdf"
-          className="border-2 border-sky-500 rounded-xl px-4 py-1 text-sky-500 hover:bg-sky-500 hover:text-white transition"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View CV
-        </a>
-        <a
-          href="/Cv folder/Mayowa-coverletter.pdf"
-          className="rounded-xl px-4 py-1 bg-sky-500 text-white hover:bg-sky-600 transition"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cover Letter
-        </a>
+          <div className="flex justify-center gap-4 mt-4">
+            <a href="https://www.linkedin.com/in/ekundayo-mayowa-5804761a4">
+              <FaLinkedin className="text-[25px] text-sky-400 hover:text-sky-300 transition" />
+            </a>
+            <a href="https://github.com/DevDistinctiveMayor">
+              <FaGithub className="text-[25px] text-slate-200 hover:text-white transition" />
+            </a>
+          </div>
+        </div>
       </div>
-
-      <div className="flex justify-center gap-4 mt-4">
-        <a href="https://www.linkedin.com/in/ekundayo-mayowa-5804761a4">
-          <FaLinkedin className="text-[25px] text-sky-400 hover:text-sky-300 transition" />
-        </a>
-        <a href="https://github.com/DevDistinctiveMayor">
-          <FaGithub className="text-[25px] text-slate-200 hover:text-white transition" />
-        </a>
-      </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
